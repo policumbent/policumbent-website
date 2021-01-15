bc7c574d393b80640ce4b4c4129d585510007625
Davide Aimar <davide.aimar99@gmail.com>
2021-01-15 23:29:46 +0100

    homepage progress

--- static_source/js/main.js
+++ static_source/js/main.js
@@ -17,57 +17,91 @@ if(document.getElementsByClassName("prototype-gallery").length > 0){
 
 // if code is in homepage
 if(document.getElementById("home-intro")){
-	var winHeight = window.innerHeight;
-	var animDuration = winHeight * 3;
-	document.getElementById('home-intro').style.height = (animDuration+winHeight) +'px';
-	var animationData;
 
-	animationData = (function () {
-		var json = null;
-		$.ajax({
-			'async': false,
-			'global': false,
-			'url': "/static/js/data.json",
-			'dataType': "json",
-			'success': function (data) {
-				json = data;
-			}
+	$(function () {
+		var height = 3500;
+		var speed = 1.5;
+		$("#home-intro").css({
+			'height': height + 'px'
+		}); 
+
+		$(window).scroll(function () {
+			
+			var window_width = $(window).width();
+			var window_height = $(window).width();
+
+			var scroll_position = $(window).scrollTop();
+			var object_position_left = window_width * (scroll_position / height);
+			var object_position_top = window_height * (scroll_position / height);
+	
+			$('#external').css({
+				'top': 0.05*speed*object_position_top + 'px'
+			});
+			// $('#back').css({
+			// 	'top': -0.2*speed*object_position_top + 'px'
+			// });
+			$('#external').css({
+				'left': 0.9*speed*object_position_left + 'px'
+			}); 
+			// $('#back').css({
+			// 	'left': -0.5*speed*object_position_left + 'px'
+			// });
 		});
-		return json;
-	})(); 
-
-	var animData = {
-		container: document.getElementById('animation-container'),
-		renderer: 'svg',
-		loop: false,
-		autoplay: false,
-		animationData: animationData,
-	};
-
-	var anim = bodymovin.loadAnimation(animData);
-	window.addEventListener('scroll', function() {
-		animatebodymovin(animDuration, anim);
 	});
 
-	function animatebodymovin(duration, animObject) {
-		var scrollPosition = window.scrollY;
-		var maxFrames = animObject.totalFrames;
-		var frame = (maxFrames / 100) * (scrollPosition / (duration / 100));
-		animObject.goToAndStop(frame, true);
-	}
 
-	function loadJSON(callback) {   
+	// OLD ANIMATION
+	// var winHeight = window.innerHeight;
+	// var animDuration = winHeight * 3;
+	// document.getElementById('home-intro').style.height = (animDuration+winHeight) +'px';
+	// var animationData;
 
-		var xobj = new XMLHttpRequest();
-		xobj.overrideMimeType("application/json");
-		xobj.open('GET', '/static/js/data.json', true);
-		xobj.onreadystatechange = function () {
-			if (xobj.readyState == 4 && xobj.status == "200") {
-				// Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-				callback(xobj.responseText);
-				console.log(xobj.responseText)
-			}
-		};
-		xobj.send(null);  
-	}
+	// animationData = (function () {
+	// 	var json = null;
+	// 	$.ajax({
+	// 		'async': false,
+	// 		'global': false,
+	// 		'url': "/static/js/data.json",
+	// 		'dataType': "json",
+	// 		'success': function (data) {
+	// 			json = data;
+	// 		}
+	// 	});
+	// 	return json;
+	// })(); 
+
+	// var animData = {
+	// 	container: document.getElementById('animation-container'),
+	// 	renderer: 'svg',
+	// 	loop: false,
+	// 	autoplay: false,
+	// 	animationData: animationData,
+	// };
+
+	// var anim = bodymovin.loadAnimation(animData);
+	// window.addEventListener('scroll', function() {
+	// 	animatebodymovin(animDuration, anim);
+	// });
+
+	// function animatebodymovin(duration, animObject) {
+	// 	var scrollPosition = window.scrollY;
+	// 	var maxFrames = animObject.totalFrames;
+	// 	var frame = (maxFrames / 100) * (scrollPosition / (duration / 100));
+	// 	animObject.goToAndStop(frame, true);
+	// }
+
+	// function loadJSON(callback) {   
+
+	// 	var xobj = new XMLHttpRequest();
+	// 	xobj.overrideMimeType("application/json");
+	// 	xobj.open('GET', '/static/js/data.json', true);
+	// 	xobj.onreadystatechange = function () {
+	// 		if (xobj.readyState == 4 && xobj.status == "200") {
+	// 			// Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+	// 			callback(xobj.responseText);
+	// 			console.log(xobj.responseText)
+	// 		}
+	// 	};
+	// 	xobj.send(null);  
+	// }
 }
