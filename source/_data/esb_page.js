6a5b66c925e5868cbfa326a6004649e78e2c651e
Davide Aimar <davide.aimar99@gmail.com>
2022-03-01 21:49:09 +0100

    add: esb page

---
+++ source/_data/esb_page.js
@@ -0,0 +1,67 @@
+const fetchFromDato = require('./api')
+
+
+module.exports = async function getWhpscPage() {
+
+    const query = `query {
+      esbPage (locale: %LOCALE%){
+        title
+        introText
+        image{
+          responsiveImage(
+              sizes: "(max-width: 768px) 100vw, (max-width: 1440px) 80vw, 1200px",
+              imgixParams: {fm: jpg, fit: crop, w: 1200, h: 500 }
+            ){
+            ...responsiveImageFragment
+          }
+        }
+        og_image: image{
+          url(imgixParams: {fm: jpg, fit: crop, w: 1200, h: 630 })
+        }
+        raceTitle
+        raceIntroText
+        raceParts{
+          name
+          description
+          image{
+            url
+            alt
+          }
+        }
+        resultsTitle
+        resultsIntroText
+        raceEditions{
+          title
+          text
+          gallery{
+            url
+            alt
+          }
+        }
+      }
+    }
+    
+    fragment responsiveImageFragment on ResponsiveImage {
+      srcSet
+      webpSrcSet
+      sizes
+      src
+      width
+      height
+      aspectRatio
+      alt
+      base64
+  }`
+
+
+
+  const response_it = await fetchFromDato(query.replace('%LOCALE%', 'it'));
+  const response_en = await fetchFromDato(query.replace('%LOCALE%', 'en'));
+  
+  const whpscPage = {
+    it: response_it.data.esbPage,
+    en: response_en.data.esbPage
+  }
+  
+  return whpscPage;
+}
